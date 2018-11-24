dca25fa3ed302e3c13afde93ce9b5af7b62a5aa2
Karan Singhal <karan1149@gmail.com>
2018-11-24 14:57:06 -0500

    Fixed spotify assignment, added weighted assignment

--- model.py
+++ model.py
@@ -9,12 +9,25 @@ def get_spotify_assignment(streams):
 		artist_counts[stream['artist']] += 1
 
 	for artist in artist_counts:
-		artist_counts[artist] /= total_count
+		artist_counts[artist] /= float(total_count)
 
 	return artist_counts
 
 def get_weighted_assignment(streams, weights):
-	pass
+	artist_weighted_counts = defaultdict(float)
+	total_weighted_counts = 0.0
+
+	for stream in streams:
+		for key in stream:
+			if key in weights:
+				artist_weighted_counts[stream['artist']] += weights[key] * stream[key]
+				total_weighted_counts += weights[key] * stream[key]
+
+	for artist in artist_weighted_counts:
+		artist_weighted_counts[artist] /= total_weighted_count
+
+	return artist_weighted_counts
+
 
 def get_groundtruth_assignment(fanbases):
 	artist_counts = defaultdict(int)
@@ -35,4 +48,9 @@ if __name__=='__main__':
 		data = json.load(f)
 
 	spotify_assignment = get_spotify_assignment(data['streams'])
+
+	weights = pass
+	weighted_assignment = get_weighted_assignment(data['streams'], weights)
+
+	groundtruth_assignment = get_groundtruth_assignment(data['fanbases'])
 
